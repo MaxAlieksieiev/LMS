fa3f75841eb359dec1f5ca6c58f3ba95eb4414cc
Max A <corp.workMA@gmail.com>
2024-10-08 22:27:25 +0300

    add dark palette & theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { AuthRoutingModule } from "./modules/auth/routing";
 import { AuthLayout } from "./modules/auth/layout";
 import { Dashboard } from "./modules/dashboard/pages/dashboard";
 import { RouteGuard } from "./modules/core/routing/routeGuards";
+import { ChakraProvider } from "@chakra-ui/react";
+import { lightTheme } from "./modules/core/configuration/theme";
 
 const router = createBrowserRouter([
   {
@@ -32,7 +34,11 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
+  return (
+    <ChakraProvider theme={lightTheme}>
+      <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
+    </ChakraProvider>
+  );
 }
 
 export default App;
