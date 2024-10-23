1617ffd9b5a56f62760e0488bf794bf3ed147fb7
Max A <corp.workMA@gmail.com>
2024-10-23 22:26:57 +0300

    add comments

--- src/modules/core/configuration/validation.ts
+++ src/modules/core/configuration/validation.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import * as Yup from "yup";
 import { VALIDATION_EMAIL_PATTERN } from "../constants/common";
 
 Yup.addMethod<Yup.StringSchema>(Yup.string, "isValidEmail", function (message) {
+  //@ts-expect-error
   return Yup.string().test({
     name: "isValidEmail",
     message,
